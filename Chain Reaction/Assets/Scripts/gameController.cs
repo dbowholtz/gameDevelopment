1b06fc9a59f131ba718f27a48bec83d8044221f8
Dustin Bowholtz <dbowholtz@gmail.com>
2015-09-30 15:16:34 -0500

    first revision of Chain Reaction

---
+++ Chain Reaction/Assets/Scripts/gameController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class gameController : MonoBehaviour {
+    public GameObject orb;
+    private bool loadBlown;
+    public orbController orbC;
+    Camera camera;
+    private int explosionCount;
+    private int orbsBlownUp;
+    private static int level =0;
+    private AudioSource audio;
+    public Text text;
+    private GameObject orbDumpster;
+    private GameObject temp;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+        Time.timeScale=1;
+        orbDumpster = GameObject.Find("orbDumpster");
+        level++;
+        StartCoroutine(displayText("Welecome to level " + level + "\nPop " + (Mathf.FloorToInt(level*5f*.5f)-1)+ " balls"));
+        Debug.Log(level);
+        explosionCount = 0;
+        orbsBlownUp = 0;
+        camera = GetComponent<Camera>();
+        camera.backgroundColor = Color.black;
+        loadBlown = false;
+        for (int i = 0; i < level*5; i++)
+        {
+            orb.transform.position = new Vector2(Random.Range(-4.0f, 4.0f), Random.Range(-4.0f, 4.0f));
+            
+            temp = Instantiate(orb);
+            temp.transform.parent = orbDumpster.transform;
+        }
+       
+    }
+    IEnumerator displayText(string textToDisplay)
+    {
+        text.text = textToDisplay;
+        yield return new WaitForSeconds(1);
+        text.text = "";
+    }
+	// Update is called once per frame
+	void Update () {
+        if((explosionCount==0 && loadBlown)||((int)Mathf.FloorToInt(level*5*0.5f))<=orbsBlownUp)
+        {
+            if (((int)Mathf.FloorToInt(level * 5 * 0.4f)) > orbsBlownUp)
+            {
+                text.text = "Try Again";
+                level--;
+            }
+            //Debug.Log("To Pop: "+((int)Mathf.Round(level* 5 * 0.5f)) + " " + "Popped: " + orbsBlownUp);
+            audio.Play();
+            
+            
+            Time.timeScale = 0;
+            System.Threading.Thread.Sleep(1000);
+            Application.LoadLevel("theScene");
+        }
+        if (Input.GetMouseButtonDown(0) && !loadBlown) 
+        {
+            loadBlown = true;
+            Camera camera = GetComponent<Camera>();
+            float x = Input.mousePosition.x;
+            float y = Input.mousePosition.y;
+            Vector3 test = camera.ScreenToWorldPoint(new Vector3(x, y, 0));
+            test.z = 0;
+            test.x += .2f;
+            test.y -= .2f;
+            orbC.gameObject.transform.position = test;
+            //orbC = orb.GetComponent<orbController>();
+            orbC.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            orbC.makeBomb();
+        }
+	}
+    public void explosion()
+    {
+        //Debug.Log("EXPLOSION");
+        orbsBlownUp++;
+        explosionCount++;
+    }
+    public void doneExploding()
+    {
+        explosionCount--;
+    }
+}
---
+++ Chain Reaction/Assets/Scripts/orbController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class orbController : MonoBehaviour {
+    Rigidbody2D orbRB;
+    public float speed;
+    public bool isBomb;
+    public float scale;
+    private Vector3 scaleVector;
+    private Color[] colorChoice = new Color[] { Color.red, Color.blue, Color.green, Color.magenta, Color.yellow };
+    private Renderer renderer;
+    private bool blownUp;
+    private Vector2 lastVelocity;
+    public gameController gc;
+    private AudioSource audio;
+    // Use this for initialization
+    void Start() {
+        //print("Orb on Start");
+        audio = GetComponent<AudioSource>();
+        gc = (gameController) GameObject.Find("Main Camera").GetComponent("gameController");
+        blownUp = false;
+        renderer = GetComponent<Renderer>();
+        int j = Random.RandomRange(0, colorChoice.Length);
+        renderer.material.color = colorChoice[j];
+        isBomb = false;
+        orbRB = GetComponent<Rigidbody2D>();
+        orbRB.velocity = new Vector2(Random.Range(-speed,speed), Random.Range(-speed,speed));
+        scaleVector = new Vector3(this.gameObject.transform.localScale.x * scale, this.gameObject.transform.localScale.y * scale, 0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //this.gameObject.layer = 0;
+        if (isBomb && this.gameObject.layer != LayerMask.NameToLayer("blownBalls"))
+        {
+            //Debug.Log(this.gameObject.layer == LayerMask.NameToLayer("blownBalls"));
+            gc.explosion();
+            orbRB.velocity = new Vector2(0.0f, 0.0f);
+            this.gameObject.layer = LayerMask.NameToLayer("blownBalls");
+            orbRB.isKinematic = true;
+            StartCoroutine(Fade());
+            audio.Play();
+        }
+
+        if (isBomb && this.gameObject.transform.localScale.x < scaleVector.x && !blownUp)
+        {
+            this.gameObject.transform.localScale += new Vector3((Time.deltaTime) * this.gameObject.transform.localScale.x, (Time.deltaTime) * this.gameObject.transform.localScale.y, 0);
+        }
+        if(this.gameObject.transform.localScale.x > scaleVector.x)
+        {
+            StartCoroutine(destroyOrb());
+            blownUp = true;
+            scaleVector = this.gameObject.transform.localScale;
+        }
+
+        
+        if(blownUp && this.gameObject.transform.localScale.x > 0)
+        {
+            this.gameObject.transform.localScale -= new Vector3(scaleVector.x * Time.deltaTime, scaleVector.y * Time.deltaTime, 0);
+        }
+    }
+    void FixedUpdate()
+    {
+        lastVelocity = orbRB.velocity;
+    }
+    IEnumerator Fade()
+    {
+        for (float f = 1f; f >= 0; f -= 0.005f)
+        {
+            Color c = renderer.material.color;
+            c.a = f;
+            renderer.material.color = c;
+            yield return null;
+        }
+    }
+    IEnumerator destroyOrb()
+    {
+        yield return new WaitForSeconds(.75f);
+        Destroy(this.gameObject);
+        gc.doneExploding();
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        orbRB.velocity = Vector2.Reflect(lastVelocity,collision.contacts[0].normal);
+        if(collision.gameObject.name.Contains("orb"))
+        {
+            this.makeBomb();
+        }
+    }
+
+    public void makeBomb()
+    {
+        isBomb = true;
+    }
+    
+}
